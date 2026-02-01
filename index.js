cb2a5c09c99acf8b6463cf095b8d67314630c6f5
mickeyyhs-ux <mickeyyhs@gmail.com>
2026-02-01 23:06:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { exec } from "child_process";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,8 +8,20 @@ app.get("/", (req, res) => {
   res.json({ ok: true, message: "server alive" });
 });
 
-app.get("/ping", (req, res) => {
-  res.send("pong");
+app.get("/ffmpeg-version", (req, res) => {
+  exec("ffmpeg -version", (error, stdout, stderr) => {
+    if (error) {
+      return res.status(500).json({
+        ok: false,
+        error: error.message,
+        stderr,
+      });
+    }
+    res.json({
+      ok: true,
+      output: stdout.split("\n")[0],
+    });
+  });
 });
 
 app.listen(port, () => {
